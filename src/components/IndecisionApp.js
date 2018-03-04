104ba32d7ab76755fd70d44c25272e963dbc4ccd
Weston <westonedwards@gmail.com>
2018-03-04 18:43:49 -0500

    Changled selected option from alert to modal using react-modal

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from './AddOption';
 import Header from './Header';
 import Action from './Action';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
     state = {
-        options: []
+        options: [],
+        selectedOption: undefined
     };
     handleDeleteOptions = () => {
         this.setState(() => ({options: []}));
@@ -21,7 +23,16 @@ export default class IndecisionApp extends React.Component {
     handlePick = () => {
         const randomNumber = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNumber];
-        alert(option);
+        this.setState(() => ({selectedOption: option}));
+    };
+    handleClearSelectedOption = () => {
+        this.setState(() => {
+            return (
+                {
+                    selectedOption: undefined
+                }
+            )
+        });
     };
     handleAddOption = (option) => {
         if(!option) {
@@ -71,6 +82,7 @@ export default class IndecisionApp extends React.Component {
                     handleDeleteOption={this.handleDeleteOption}
                 />
                 <AddOption handleAddOption={this.handleAddOption} />
+                <OptionModal selectedOption={this.state.selectedOption} handleClearSelectedOption={this.handleClearSelectedOption} />
             </div>
         );
     }
