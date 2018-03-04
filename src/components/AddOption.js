f351cff563d118aa6367443e8fe0f393d591a269
Weston <westonedwards@gmail.com>
2018-03-04 12:49:02 -0500

    Updated classes to use ES6 methods

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
 export default class AddOption extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.state = {
-            error: undefined
-        }
-    }
-    handleAddOption(event) {
+    state = {
+        error: undefined
+    };
+    handleAddOption = (event) => {
         event.preventDefault();
         const option = event.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
