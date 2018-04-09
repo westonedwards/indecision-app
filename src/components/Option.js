278bcb17fade32645e996fbde7300f263a268def
Weston <westonedwards@gmail.com>
2018-04-08 20:40:58 -0400

    Revert "Updated app name"
    This reverts commit 1f7044aff3d4bdfd698ac2a0a691c9c56c91c792.

---
+++ src/components/Option.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Option = (props) => {
+    return (
+        <div>
+            {props.optionText}
+            <button
+                className="button button--link" 
+                onClick={(e) => {props.handleDeleteOption(props.optionText);}}
+            >Remove</button>
+        </div>
+    );
+};
+
+export default Option;
